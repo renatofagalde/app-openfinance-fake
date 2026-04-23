e75affe05e7af3942b6c168d54a234d00e8c9a69
Renato Fagalde <renato@likwi.com.br>
2026-04-23 17:13:14 -0300

    Add JSON tags to `Consentimento` domain model fields

--- internal/application/domain/consentimento.go
+++ internal/application/domain/consentimento.go
@@ -1,8 +1,8 @@
 package domain
 
 type Consentimento struct {
-	ConsentId     string `dynamodbav:"consent_id"`
-	ConsentStatus string `dynamodbav:"consent_status"`
+	ConsentId     string `dynamodbav:"consent_id"     json:"consent_id"`
+	ConsentStatus string `dynamodbav:"consent_status" json:"consent_status"`
 }
 
 const (
