926f0173ed0406a84d8beca550a7b75939f6e024
Renato Fagalde <renato@likwi.com.br>
2026-04-23 17:13:20 -0300

    Add JSON tags to `Permissao` domain model fields

--- internal/application/domain/permissao.go
+++ internal/application/domain/permissao.go
@@ -1,7 +1,7 @@
 package domain
 
 type Permissao struct {
-	ConsentId  string `dynamodbav:"consent_id"`
-	Permission string `dynamodbav:"permission"`
-	Lancar403  bool   `dynamodbav:"lancar_403"`
+	ConsentId  string `dynamodbav:"consent_id"  json:"consent_id"`
+	Permission string `dynamodbav:"permission"  json:"permission"`
+	Lancar403  bool   `dynamodbav:"lancar_403"  json:"lancar_403"`
 }
